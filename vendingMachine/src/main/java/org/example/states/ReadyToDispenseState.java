328941d70b8242636c90ad9fc4cddd5f6d610471
dhawalpatil200 <dhawalpatil200@gmail.com>
2026-01-18 19:32:45 +0530

    added vending machine code

---
+++ vendingMachine/src/main/java/org/example/states/ReadyToDispenseState.java
@@ -0,0 +1,35 @@
+package org.example.states;
+
+import org.example.VendingMachine;
+
+import java.math.BigDecimal;
+
+public class ReadyToDispenseState extends VendingMachineState {
+
+    public ReadyToDispenseState(VendingMachine vendingMachine) {
+        super(vendingMachine);
+    }
+
+    @Override
+    public void selectProduct(String rackCode) {
+        System.out.println("Product already selected");
+    }
+
+    @Override
+    public void insertMoney(BigDecimal amount) {
+        System.out.println("Payment already completed");
+    }
+
+    @Override
+    public void dispense() {
+        vendingMachine.setDispensingState();
+        vendingMachine.dispense();
+    }
+
+    @Override
+    public void cancel() {
+        vendingMachine.refund();
+        vendingMachine.reset();
+        vendingMachine.setIdleState();
+    }
+}
